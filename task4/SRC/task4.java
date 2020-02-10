f25c8e0778e4586457a0082c8a2c266c9717f984
Artem-Markula <54140481+Artem-Markula@users.noreply.github.com>
2020-02-10 06:30:52 +0300

    Update task4.java

--- task4/SRC/task4.java
+++ task4/SRC/task4.java
@@ -23,48 +23,67 @@ public class task4 {
         Formatter save = new Formatter(file);
 
         String h[] = new String[3];
-        Integer A[] = new Integer[100];
+        String a[] = new String[3];
         Integer B[] = new Integer[100];
         Integer C[] = new Integer[100];
+
         if (args.length < 2) {
             System.out.println("Указана одна или не указана не одна строка в виде аргумента, укажите 2 аргумента");
 
         } else {
             for (String str : args) {
 
+
                 try (Scanner sc = new Scanner(str)) {
 
-                    while (sc.hasNext()) {
-                        //System.out.println(sc.findInLine("\\*+"));
-                        //System.out.println(sc.findInLine("(\\*+)(\\w+)"));
-                        //System.out.println(sc.findInLine("(\\w+)(.*+)"));
 
+                    while (sc.hasNext()) {
                         g = g + 1;
                         String j = sc.next();
                         h[g] = j;
+                        a[g] = j;
+                        x = a[2].replaceAll("[^\\*]", "");
 
-                }
+                        for (s = 0; s < x.length(); s++){
+                            m = m + 1;
+                            if (m == 0){
+                                System.out.println("ОК");
+                            }
+                            if (m < 10){
+                                h[1] = h[2];
+
+                            }
+                        }
+
+                        if (h[1].equals(h[2]) == true){
+                            System.out.println("ОК");
+                            break;
+                        }
+
+
                     List<String> list = new ArrayList<String>(Arrays.asList(h));
                     list.removeAll(Arrays.asList(":", null));
 
-                    if (list.get(1).equals("com") == true) {
+
+                    }
+
+                    if (h[2].equals("com") == true) {
                         System.out.println("ОК");
                         break;
                     }
 
 
-                    if (list.get(0).equals(list.get(1)) == true){
-                        System.out.println("ОК");
-                        break;
-                    }
-
-
-                    else{
+                    if (h[1].equals(h[2]) == false) {
                         System.out.println("КО");
+                        break;
                     }
+
+
+
 
 
                 } catch (Exception e) {
+
 
                 }
 
@@ -74,3 +93,5 @@ public class task4 {
             }
         }
     }
+
+
